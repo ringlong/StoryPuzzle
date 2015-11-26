f34f6f66c93ff7c0961382ded742032d22b36f61
宏宇 <266949@qq.com>
2015-11-26 22:01:29 +0800

    add move logic

--- StoryPuzzle/RRMainViewController.m
+++ StoryPuzzle/RRMainViewController.m
@@ -23,6 +23,9 @@ static const NSInteger RRImageViewColumnCount = 3;
 @property (nonatomic, strong) UIView *plactholder;
 @property (nonatomic, strong) RRImageView *imageView;
 
+@property (nonatomic, strong) NSArray<UIView *> *originalImageViewList;
+@property (nonatomic, strong) NSMutableArray<UIView *> *currentImageViewList;
+
 - (void)imageView:(UIImageView *)imageView changeWithDirection:(RRMoveDirection)direction;
 
 - (void)changeFrameOfView:(UIView *)view withView:(UIView *)otherView;
@@ -37,6 +40,7 @@ static const NSInteger RRImageViewColumnCount = 3;
     [super viewDidLoad];
     
     NSArray<UIImage *> *imageList = [RRImageKit separateImage:[UIImage imageNamed:@"Sweat"] byRows:RRImageViewRowCount columns:RRImageViewColumnCount cacheQuality:1];
+    _currentImageViewList = [NSMutableArray arrayWithCapacity:imageList.count];
     [imageList enumerateObjectsUsingBlock:^(UIImage * _Nonnull image, NSUInteger idx, BOOL * _Nonnull stop) {
         RRImageView *itemImage = [[RRImageView alloc] initWithImage:image];
         itemImage.delegate = self;
@@ -51,9 +55,12 @@ static const NSInteger RRImageViewColumnCount = 3;
         if (idx != RRImageViewRowCount * RRImageViewColumnCount - 1) {
             itemImage.frame = frame;
             [self.containerView addSubview:itemImage];
+            [_currentImageViewList addObject:itemImage];
         } else {
             self.plactholder = [[UIView alloc] initWithFrame:frame];
+            self.plactholder.tag = RRImageViewTag + idx;
             [self.containerView addSubview:self.plactholder];
+            [_currentImageViewList addObject:self.plactholder];
         }
     }];
 }
@@ -61,17 +68,49 @@ static const NSInteger RRImageViewColumnCount = 3;
 #pragma mark - Private Methods
 
 - (void)imageView:(UIImageView *)imageView changeWithDirection:(RRMoveDirection)direction {
-    [UIView animateWithDuration:0.3f animations:^{
-        if (direction != RRMoveDirectionNone) {
+    if (direction != RRMoveDirectionNone) {
+        [UIView animateWithDuration:0.3 animations:^{
             [self changeFrameOfView:imageView withView:self.plactholder];
-        }
-    }];
+        } completion:^(BOOL finished) {
+            if (finished) {
+                __block BOOL isSuccessed = YES;
+                [_currentImageViewList enumerateObjectsUsingBlock:^(UIView * _Nonnull obj, NSUInteger idx, BOOL * _Nonnull stop) {
+                    if (idx + 1 < _currentImageViewList.count) {
+                        UIView *nextObject = _currentImageViewList[idx + 1];
+                        if (obj.tag > nextObject.tag) {
+                            isSuccessed = NO;
+                        }
+                    }
+                }];
+                
+                if (isSuccessed) {
+                    UIAlertController *alert = [UIAlertController alertControllerWithTitle:@"老婆" message:@"我爱你=。=" preferredStyle:UIAlertControllerStyleAlert];
+                    UIAlertAction *action = [UIAlertAction actionWithTitle:@"OK" style:UIAlertActionStyleDefault handler:^(UIAlertAction * _Nonnull action) {
+                        [alert dismissViewControllerAnimated:YES completion:nil];
+                    }];
+                    [alert addAction:action];
+                    [self presentViewController:alert animated:YES completion:nil];
+                }
+            }
+            
+        }];
+    }
 }
 
 - (void)changeFrameOfView:(UIView *)view withView:(UIView *)otherView {
     CGRect frame = view.frame;
     view.frame = otherView.frame;
     otherView.frame = frame;
+    
+    // 交换在数组中的位置
+    NSInteger originalIndex = [_currentImageViewList indexOfObject:view];
+    NSInteger otherIndex = [_currentImageViewList indexOfObject:otherView];
+    
+    [_currentImageViewList removeObject:otherView];
+    [_currentImageViewList insertObject:otherView atIndex:originalIndex];
+
+    [_currentImageViewList removeObject:view];
+    [_currentImageViewList insertObject:view atIndex:otherIndex];
 }
 
 #pragma mark - RRImageViewDelegate
